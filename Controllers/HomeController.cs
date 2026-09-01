8b16334b0b90e8e32249a66087e3ae42c3d1d0c6
Genar0Islas <164415634+Genar0Islas@users.noreply.github.com>
2026-09-01 16:12:49 -0700

    Creacion de tablas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,9 @@ namespace Portafolio.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
         }
 
         public IActionResult Privacy()
@@ -20,6 +22,38 @@ namespace Portafolio.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private List<Proyecto> ObtenerProyectos() {
+            return new List<Proyecto>() { new Proyecto 
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-conmerce realizado en ASP.NET core",
+                    Link = "https://amazon.com",
+                    ImagenURL = "/images/amazon.png"
+                }, 
+                new Proyecto 
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Pagina de noticias en React",
+                    Link = "https://nytimes.com",
+                    ImagenURL = "/images/nytimes.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social de noticias y contenido",
+                    Link = "https://reddit.com",
+                    ImagenURL = "/images/reddit.png"
+                },
+                new Proyecto 
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en linea para comprar videojuegos",
+                    Link = "https://store.steampowered.com",
+                    ImagenURL = "/images/steam.png"
+                },
+            };
         }
     }
 }
